6d0c2186aa10acaecd237f2bad90b3e664b8f2e1
Nami Sunami <nsunami@pm.me>
2024-05-25 14:16:30 +0200

    Update Home to draw the graph

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-
-import { Form, Label, RadioField, Submit } from '@redwoodjs/forms'
 import { Metadata } from '@redwoodjs/web'
 
 import ItemsCell from '../../components/ItemsCell'
+import ItemsOverTimeCell from '../../components/ItemsOverTimeCell'
 
 const ITEM_TYPES = {
   DATASET: '3',
@@ -11,25 +9,11 @@ const ITEM_TYPES = {
 }
 
 const HomePage = () => {
-  const [type, setType] = useState(ITEM_TYPES.DATASET)
-  function onSubmit(data) {
-    setType(data.type)
-  }
   return (
     <>
       <Metadata title="Home" description="Home page" />
-      <Form onSubmit={onSubmit}>
-        <RadioField name="type" value={ITEM_TYPES.DATASET} />
-        <Label name="type" htmlFor={'dataset'}>
-          Dataset
-        </Label>
-        <RadioField name="type" value={ITEM_TYPES.SOFTWARE} />
-        <Label name="type" htmlFor="software">
-          Software
-        </Label>
-        <Submit>Go</Submit>
-      </Form>
-      <ItemsCell type={type.valueOf()} />
+      <ItemsOverTimeCell timeFrame="year" />
+      <ItemsCell />
     </>
   )
 }
