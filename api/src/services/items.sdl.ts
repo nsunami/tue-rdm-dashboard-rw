5ca438c2ee5963a986ab4d7b7c2f4d623af7d9ae
Nami Sunami <nsunami@pm.me>
2024-05-25 14:15:47 +0200

    Add services to aggregates over time

--- api/src/services/items.sdl.ts
+++ api/src/services/items.sdl.ts
@@ -3,3 +3,21 @@ import { db } from 'src/lib/db'
 export function Items() {
   return db.item.findMany({ include: { license: true } })
 }
+
+type TimeFrameType = 'week' | 'month' | 'year'
+
+export function itemsOverTime({
+  timeFrame = 'week',
+}: {
+  timeFrame: TimeFrameType
+}) {
+  const result = db.$queryRaw`
+  SELECT
+    DATE_TRUNC(${timeFrame}, published_date) AS date,
+    COUNT(*) as count
+  FROM "Item"
+  GROUP BY date ORDER BY date
+  `
+
+  return result
+}
