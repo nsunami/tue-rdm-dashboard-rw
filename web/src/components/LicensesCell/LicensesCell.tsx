bccb2a4007f1249317837f1517520b92d8654ee0
Nami Sunami <nsunami@pm.me>
2024-05-27 22:34:05 +0200

    Show percentage for licenses

--- web/src/components/LicensesCell/LicensesCell.tsx
+++ web/src/components/LicensesCell/LicensesCell.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { BarList, Card } from '@tremor/react'
 import type { LicensesQuery, LicensesQueryVariables } from 'types/graphql'
 
@@ -28,13 +30,31 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({ licenseCounts }: CellSuccessProps<LicensesQuery>) => {
+export const Success = ({
+  licenseCounts,
+  showPercent: defaultShowPercent = false,
+}: CellSuccessProps<LicensesQuery>) => {
+  const [showPercent, setShowPercent] = useState(defaultShowPercent)
+  const itemsTotal = licenseCounts.reduce(
+    (accumulator, current) => accumulator + current.count,
+    0
+  )
+
   return (
     <>
       <Card>
         <BarList
+          onClick={() => setShowPercent((c) => !c)}
           color={'slate-300'}
-          data={licenseCounts.map((e) => ({ ...e, value: e.count }))}
+          data={licenseCounts.map((e) => {
+            if (showPercent)
+              return { ...e, value: ((e.count / itemsTotal) * 100).toFixed(1) }
+            return { ...e, value: e.count }
+          })}
+          valueFormatter={(v) => {
+            if (showPercent) return `${v}%`
+            return v
+          }}
         />
       </Card>
     </>
