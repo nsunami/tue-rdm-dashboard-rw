39597a96473705a7e500820b33c0beff2e6b289b
Nami Sunami <nsunami@pm.me>
2024-05-27 15:44:37 +0200

    Remove the datasets from the homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,12 @@
 import { Metadata } from '@redwoodjs/web'
 
-import ItemsCell from '../../components/ItemsCell'
 import ItemsOverTimeCell from '../../components/ItemsOverTimeCell'
-
-const ITEM_TYPES = {
-  DATASET: '3',
-  SOFTWARE: '9',
-}
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
       <ItemsOverTimeCell timeFrame="year" />
-      <ItemsCell />
     </>
   )
 }
