c0b17c28f295dc34fd468442ad4ba58c80f386bf
Nami Sunami <nsunami@pm.me>
2024-05-28 18:31:58 +0200

    Use Card component in Items

--- web/src/components/Item/Item.tsx
+++ web/src/components/Item/Item.tsx
@@ -1,3 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
+
 const Item = ({ item }) => {
   const { title, description, license, url_public_html, published_date } = item
   const parsedDescription = new DOMParser().parseFromString(
@@ -10,16 +18,22 @@ const Item = ({ item }) => {
   }
 
   return (
-    <div className="my-1 border">
-      <h2 className="font-bold">
-        <a href={url_public_html} target="_blank" rel="noopener noreferrer">
-          {title}
-        </a>
-      </h2>
-      <div>{license.name}</div>
-      <div>{formatDate(published_date)}</div>
-      <p className="line-clamp-2">{parsedDescription}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <a href={url_public_html} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </CardTitle>
+        <CardDescription className="flex flex-row place-content-between">
+          <div>{license.name}</div>
+          <div>{formatDate(published_date)}</div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="line-clamp-2">{parsedDescription}</p>
+      </CardContent>
+    </Card>
   )
 }
 
