849c18127c3d9fd7ec964e6d50b4a621e136cbc3
Nami Sunami <nsunami@pm.me>
2024-05-28 21:07:21 +0200

    Remove title from the Datasets page

--- web/src/pages/DatasetsPage/DatasetsPage.tsx
+++ web/src/pages/DatasetsPage/DatasetsPage.tsx
@@ -6,8 +6,6 @@ const DatasetsPage = () => {
   return (
     <>
       <Metadata title="Datasets" description="Datasets page" />
-
-      <h1 className="text-xl">Datasets</h1>
       <ItemsCell />
     </>
   )
