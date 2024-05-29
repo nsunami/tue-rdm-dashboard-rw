80cb8beeb0ab07a7bbfff20f3ea9f63f405396f9
Nami Sunami <nsunami@pm.me>
2024-05-29 22:02:44 +0200

    Add total number of items and a short About section

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,14 @@
 import { Metadata } from '@redwoodjs/web'
 
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from 'src/components/ui/card'
+
 import ItemsOverTimeCell from '../../components/ItemsOverTimeCell'
+import ItemsTotalCell from '../../components/ItemsTotalCell'
 import LicensesCell from '../../components/LicensesCell'
 
 const HomePage = () => {
@@ -8,6 +16,24 @@ const HomePage = () => {
     <>
       <Metadata title="Home" description="Home page" />
       <section className="grid md:grid-cols-2 md:gap-2 md:px-2">
+        <Card>
+          <CardHeader>
+            <CardTitle>4TU.ResearchData Dashboard</CardTitle>
+          </CardHeader>
+          <CardContent>
+            This dashboard shows the metrics for datasets posted on{' '}
+            <a
+              href="https://data.4tu.nl"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:underline"
+            >
+              4TU.ResearchData
+            </a>{' '}
+            from the TU/e research community.
+          </CardContent>
+        </Card>
+        <ItemsTotalCell />
         <ItemsOverTimeCell timeFrame="year" />
         <LicensesCell />
       </section>
