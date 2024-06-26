f142cdbc6f519ff94df1d7c6b57d07390d02c41c
Nami Sunami <nsunami@pm.me>
2024-06-26 14:08:40 +0200

    Implement the Leadearboard to the Authors tab

--- web/src/pages/AuthorsPage/AuthorsPage.tsx
+++ web/src/pages/AuthorsPage/AuthorsPage.tsx
@@ -1,19 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+import AuthorLeaderboardCard from 'src/components/AuthorLeaderboardCard/AuthorLeaderboardCard'
 const AuthorsPage = () => {
   return (
     <>
       <Metadata title="Authors" description="Authors page" />
 
-      <h1>AuthorsPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AuthorsPage/AuthorsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>authors</code>, link to me with `
-        <Link to={routes.authors()}>Authors</Link>`
-      </p>
+      <AuthorLeaderboardCard />
     </>
   )
 }
